84ed6db885151d68a06770396cdabed1c205145b
Yashsingh Chauhan <107561263+AIZEN-Hado@users.noreply.github.com>
2026-05-04 13:28:27 +0530

    Fix console log indentation in index.js

--- server/index.js
+++ server/index.js
@@ -85,6 +85,5 @@ app.use((err, req, res, next) => {
 });
 
 app.listen(port, () => {
-        console.log(`Server started on port ${port}`);
-    }
+    console.log(`Server started on port ${port}`);
 });
